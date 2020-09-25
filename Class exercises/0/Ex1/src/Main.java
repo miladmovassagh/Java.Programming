8e56be6a087947c6c2d11718b67c591327506561
Milad Movassagh <miladmovassagh62@gmail.com>
2020-09-25 18:12:26 +0330

    add new project

---
+++ Class exercises/0/Ex1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int i, j;
+        for(i = 0; i < 4; i++) {
+            for(j = 0; j <= i; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
