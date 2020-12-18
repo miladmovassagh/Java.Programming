0e5acb36b5312e47bc51eed143b71487996849e1
Milad Movassagh <miladmovassagh62@gmail.com>
2020-12-18 21:53:32 +0330

    add new project

--- Homework/11/Ex1/src/com/usc/Students.java
+++ Homework/11/Ex1/src/com/usc/Students.java
@@ -1,32 +1,52 @@
 package com.usc;
 
 import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
-public class Students {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
-    public Students() throws Exception {
-        JSONObject jsObject1 = new JSONObject();
-        jsObject1.put("name","milad");
-        jsObject1.put("family","movassagh");
-        jsObject1.put("grade","19");
-        JSONObject jsObject2 = new JSONObject();
-        jsObject2.put("name","reza");
-        jsObject2.put("family","askari");
-        jsObject2.put("grade","15.5");
-        JSONObject jsObject3 = new JSONObject();
-        jsObject3.put("name","ali");
-        jsObject3.put("family","bahrami");
-        jsObject3.put("grade","17");
-        JSONObject jsObject4 = new JSONObject();
-        jsObject4.put("name","saeed");
-        jsObject4.put("family","saadati");
-        jsObject4.put("grade","16.5");
-        /*JSONArray jsonArray = new JSONArray();
-        jsonArray.put(jsObject1);
-        jsonArray.put(jsObject2);
-        jsonArray.put(jsObject3);
-        jsonArray.put(jsObject4);
-         */
+@Path("/save")
+public class Students {
+    @Path("/students")
+    @GET
+    @Produces("text/plain")
+    public String students() throws JSONException {
+        System.out.println("save executed in server");
+        JSONObject jsonObject1 = new JSONObject();
+        jsonObject1.put("name","milad");
+        jsonObject1.put("family","movassagh");
+        jsonObject1.put("grade","19");
+
+        JSONObject jsonObject2 = new JSONObject();
+        jsonObject2.put("name","reza");
+        jsonObject2.put("family","askari");
+        jsonObject2.put("grade","15.5");
+
+        JSONObject jsonObject3 = new JSONObject();
+        jsonObject3.put("name","ali");
+        jsonObject3.put("family","bahrami");
+        jsonObject3.put("grade","17");
+
+        JSONObject jsonObject4 = new JSONObject();
+        jsonObject4.put("name","saeed");
+        jsonObject4.put("family","saadati");
+        jsonObject4.put("grade","16.5");
+
+        JSONObject jsonObject5 = new JSONObject();
+        jsonObject5.put("name","amin");
+        jsonObject5.put("family","rigi");
+        jsonObject5.put("grade","12.5");
+
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.put(jsonObject1);
+        jsonArray.put(jsonObject2);
+        jsonArray.put(jsonObject3);
+        jsonArray.put(jsonObject4);
+        jsonArray.put(jsonObject5);
+
+        return jsonArray.toString();
     }
 }
